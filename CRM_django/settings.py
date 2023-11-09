52ab8824ada57a1367b8d6badd1565d071ce6316
“Arian” <arian09337332108@gmail.com>
2023-11-09 15:52:38 +0330

    finished working on the models and the data base and craeting a super user / just tested soo many things and debugged a lot

--- CRM_django/settings.py
+++ CRM_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leads'
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+AUTH_USER_MODEL = 'leads.User'
 ROOT_URLCONF = 'CRM_django.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- leads/models.py
+++ leads/models.py
@@ -1,18 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# better to create your own user b for bigger projects it becomes a big problem
+
+
+# this thing is editable and we can add anything we want to it
+
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
 
 
 class lead(models.Model):
-    SOURCE_CHOICE = (
-        ('Youtube', 'Youtube'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICE, max_length=100)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
-
+# SOURCE_CHOICE = (
+#     ('Youtube', 'Youtube'),
+#     ('Google', 'Google'),
+#     ('Newsletter', 'Newsletter'),
+# )
+# phoned = models.BooleanField(default=False)
+# source = models.CharField(choices=SOURCE_CHOICE, max_length=100)
+#
+# profile_picture = models.ImageField(blank=True, null=True)
+# special_files = models.FileField(blank=True, null=True)
